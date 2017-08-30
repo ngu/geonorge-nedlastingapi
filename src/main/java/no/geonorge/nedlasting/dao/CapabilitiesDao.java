197ff09c2c5d757cc61958a2f1072be9cd5fa61a
Tore Halset <halset@pvv.ntnu.no>
2017-08-30 23:21:30 +0200

    remove previous dao

--- src/main/java/no/geonorge/nedlasting/dao/CapabilitiesDao.java
+++
@@ -1,9 +0,0 @@
-package no.geonorge.nedlasting.dao;
-
-import no.geonorge.skjema.sosi.tjenestespesifikasjon.nedlastingapi._2.CapabilitiesType;
-
-public interface CapabilitiesDao {
-		
-	public CapabilitiesType getCapabilities(String metadatauuid);
-
-}
