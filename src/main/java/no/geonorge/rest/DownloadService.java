e77e74f8cf47c713321878ee1255fb8cbade64b0
Bjørn Ove Grøtan <bjorn.ove.grotan@ngu.no>
2019-01-10 09:36:53 +0100

    Update taxonomyUrl to machine readable XML endpoint

--- src/main/java/no/geonorge/rest/DownloadService.java
+++ src/main/java/no/geonorge/rest/DownloadService.java
@@ -718,14 +718,14 @@ public class DownloadService {
             
             SyndCategory formatCategory = new SyndCategoryImpl();
             formatCategory.setName("Format:" + datasetFile.getFormatName());
-            formatCategory.setTaxonomyUri("https://register.geonorge.no/metadata-kodelister/vektorformater");
+            formatCategory.setTaxonomyUri("https://register.geonorge.no/api/metadata-kodelister/vektorformater.xml");
             categories.add(formatCategory);
 
             SyndCategory areaCategory = new SyndCategoryImpl();
             areaCategory.setName(datasetFile.getAreaType());
             // Support category labels. Requires patched rometools from https://github.com/bgrotan/rome/tree/syndcategorylabel
             areaCategory.setLabel(datasetFile.getAreaName());
-            areaCategory.setTaxonomyUri("https://register.geonorge.no/metadata-kodelister/geografisk-distribusjonsinndeling");
+            areaCategory.setTaxonomyUri("https://register.geonorge.no/api/metadata-kodelister/geografisk-distribusjonsinndeling.xml");
             categories.add(areaCategory);
 
             SyndCategory crsCategory = new SyndCategoryImpl();
